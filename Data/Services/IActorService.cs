0be7ffae73ae89d9aea8e34e483e394473d5498b
Viththiyakaran <battikaran6@gmail.com>
2022-07-01 00:52:32 +0530

    Add the first IActorService

---
+++ Data/Services/IActorService.cs
@@ -0,0 +1,22 @@
+﻿using eCommerce_project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce_project.Data.Services
+{
+     public interface IActorService
+    {
+        IEnumerable<Actor> GetAl();
+
+        Actor GetById(int id);
+
+        void Add(Actor actor);
+
+        Actor Update(int id, Actor newActor);
+
+        void Delete(int id);
+
+    }
+}
